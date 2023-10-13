59d690750ddc9d64b6b89c9082eb070cc1e1e98d
wberaldo <welberaldo@gmail.com>
2023-10-13 20:35:26 -0300

    first commit

---
+++ src/main/java/br/com/wellingtonberaldo/todolist/task/ITaskRepository.java
@@ -0,0 +1,15 @@
+package br.com.wellingtonberaldo.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID idUser);
+
+    //TaskModel findByIdAndByIdUser(UUID id, UUID idUser);
+    
+}
